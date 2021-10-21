c6a053434b649b9431452cda46b58a77e66ac501
Irakli <koregm@gmail.com>
2021-10-21 21:16:51 +0400

    Adding Other Components

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <p>Copyright 2021</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ comps/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h1>Ninja List</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Ninja Listing</a>
+    </nav>
+  );
+}
+
+export default Navbar;
