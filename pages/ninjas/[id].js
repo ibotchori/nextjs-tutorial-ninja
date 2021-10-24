63ae4b0f8efdfe2df3a62fdbded38ef58e14dbd8
Irakli <koregm@gmail.com>
2021-10-24 13:05:55 +0400

    Dynamic Routes (part 2 - getStaticPaths)

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,22 @@
+// This special  function runs before component render, it returns all the possible values for route parameter
+export const getStaticPaths = async () => {
+const res = await fetch('https://jsonplaceholder.typicode.com/users')
+const data = await res.json()
+
+// get paths from date
+const paths = data.map(user => {
+    return {
+        params: {id: user.id.toString()}
+    }
+})
+return {
+    paths, // set paths to the property
+    fallback: false // show 404 page if route does not exist
+}
+} 
+
+
+
 const Details = () => {
   return (
     <div>
